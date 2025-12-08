0f55a59fb8c0d83eae0be004ddb41c40a7629a26
hilinaayenew <hilinaayenew47@gmail.com>
2025-12-08 14:29:45 +0300

    Update progactionsformidfielder.py

--- progactionsformidfielder.py
+++ progactionsformidfielder.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_icon="⚽",
     layout="wide",       # This makes the app full width
     initial_sidebar_state="expanded",
-    
+    background-color="white",
 )
 
 # -----------------------
